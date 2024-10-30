680c3b66eae0b34fc8d20c027e3071c0c9132bd3
AleZandrox <brian.palencia0550@gmail.com>
2024-10-30 15:44:31 +0100

    Task 3.1 commit

--- module_3/task_3-2/task_3-2.mjs
+++ module_3/task_3-2/task_3-2.mjs
@@ -4,6 +4,26 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+const answerNumber = 45;
+let guessNumber = 0;
+while(answerNumber !== guessNumber){
+    guessNumber = Math.ceil(Math.random() * 60);
+}
+printOut("Guess Number = " + guessNumber.toString());
+printOut(" ");
+
+guessNumber = 0;
+let guessCount = 0;
+const startTime = Date.now();
+while(answerNumber !== guessNumber){
+    guessCount++;
+    guessNumber= Math.ceil(Math.random() * 1000000);
+}
+const endTime = Date.now();
+const timeUsed = endTime - startTime;
+printOut("Antall gjetninger " + guessCount.toString() + " tok " + timeUsed.toString() + " ms");
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
@@ -34,6 +54,54 @@ printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+printOut("Dice Rolling Extravaganza")
+
+const d1 = Math.ceil(Math.random() * 6);
+const d2 = Math.ceil(Math.random() * 6);
+const d3 = Math.ceil(Math.random() * 6);
+const d4 = Math.ceil(Math.random() * 6);
+const d5 = Math.ceil(Math.random() * 6);
+const d6 = Math.ceil(Math.random() * 6);
+
+let diceThrow = "";
+diceThrow += d1.toString() + ",";
+diceThrow += d2.toString() + ",";
+diceThrow += d3.toString() + ",";
+diceThrow += d4.toString() + ",";
+diceThrow += d5.toString() + ",";
+diceThrow += d6.toString();
+
+printOut("diceThrow: " + diceThrow);
+
+const count1 = (diceThrow.match(/1/g) || "").length;
+const count2 = (diceThrow.match(/2/g) || "").length;
+const count3 = (diceThrow.match(/3/g) || "").length;
+const count4 = (diceThrow.match(/4/g) || "").length;
+const count5 = (diceThrow.match(/5/g) || "").length;
+const count6 = (diceThrow.match(/6/g) || "").length;
+
+let diceCount = "";
+diceCount += count1.toString()+ ",";
+diceCount += count2.toString()+ ",";
+diceCount += count3.toString()+ ",";
+diceCount += count4.toString()+ ",";
+diceCount += count5.toString()+ ",";
+diceCount += count6.toString()+ ",";
+printOut("diceCount: " + diceCount);
+
+const equals1 = (diceCount.match(/1/g) || "").length;
+const equals6 = (diceCount.match(/6/g) || "").length;
+
+printout("equals1: " + equals1.toString());
+printOut("equals6: " + equals6.toString());
+
+if(equals1 === 6){
+    printOut("Full straight");
+}else if (equals6 === 1){
+    printOut("Yatzy!!!");
+}
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
